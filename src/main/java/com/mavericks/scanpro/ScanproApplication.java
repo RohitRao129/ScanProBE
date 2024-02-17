aa5de4fd309a85828ed3ea3a0e265b6e3b0f24d8
RohitRao129 <rohityada129@gmail.com>
2024-02-17 21:52:17 +0530

    Starting project

---
+++ src/main/java/com/mavericks/scanpro/ScanproApplication.java
@@ -0,0 +1,13 @@
+package com.mavericks.scanpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mavericks/scanpro/ScanproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavericks.scanpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
